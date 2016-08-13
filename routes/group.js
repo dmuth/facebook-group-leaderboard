67e6c1fc9e3b4d6caf71b8092a2ce0fc0d0922d3
Douglas Muth <dmuth@dmuth.org>
2016-08-13 16:34:15 -0400

    Added /group endpoint

---
+++ routes/group.js
@@ -0,0 +1,29 @@
+/**
+* Endpoint for /group
+*/
+var express = require('express');
+var router = express.Router();
+
+
+module.exports = function(fb) {
+
+
+router.get('/:group', function(req, res, next) {
+
+	var data = fb.getData();
+	var group = req.params.group;
+
+	if (!data[group]) {
+		res.status(404)
+			.send("Group not found!");
+		return(null);
+	}
+
+	res.render('group', { data: data[group] });
+
+});
+
+return(router);
+
+} // End of exports()
+
